967f4d4e88cd47cf155a1026d8b1fdb0182e9909
ttrepka <trepka.tommy@gmail.com>
2018-12-04 14:28:37 +0100

    Fix App tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import ChampionshipModal from './stats/Championship';
 import Game from './game/Game';
 import Modal from './modal/Modal';
+import rootReducer from './rootReducer';
 import Stats from './stats/Stats';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -12,14 +20,16 @@ const App = () => {
   const openModal = () => setModalOpen(true);
 
   return (
-    <>
-      <Game />
-      <Stats />
-      <button onClick={openModal}>Championship Stats</button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <ChampionshipModal />
-      </Modal>
-    </>
+    <Provider store={store}>
+      <>
+        <Game />
+        <Stats />
+        <button onClick={openModal}>Championship Stats</button>
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <ChampionshipModal />
+        </Modal>
+      </>
+    </Provider>
   );
 };
 
